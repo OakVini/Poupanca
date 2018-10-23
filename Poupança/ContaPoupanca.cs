677ba243085777cc2eb93d65e4144535b409600e
OakVini <35837589+OakVini@users.noreply.github.com>
2018-10-23 19:15:13 -0300

    Add files via upload

---
+++ Poupança/ContaPoupanca.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poupança
+{
+    class ContaPoupanca
+    {
+
+        //Mostra o saldo.
+        public void SaldoPoupanca()
+        {
+
+            Console.WriteLine("Poupador1: {0}", poupador1);
+            Console.WriteLine("Poupador2: {0}", poupador2);
+
+        }
+
+        
+
+        public double poupador1 = 2000;
+        public double poupador2 = 3000;
+      
+
+        public static double taxaDeJurosAnual = 0.04;
+
+
+        public double CalcularJurosMensal()
+        {
+
+            Console.WriteLine("\n");
+
+            var CalculandoPoup1 = (poupador1 * taxaDeJurosAnual) / 12;
+            var novoSaldoPoupador1 = poupador1 + CalculandoPoup1;
+            poupador1 = novoSaldoPoupador1;
+
+            var CalculandoPoup2 = (poupador2 * taxaDeJurosAnual) / 12;
+            var novoSaldoPoupador2 = poupador2 + CalculandoPoup2;
+            poupador2 = novoSaldoPoupador2;
+
+
+            return (CalculandoPoup1 + CalculandoPoup1);
+            
+
+        }
+        
+
+
+        public double AlterarTaxaDeJuros()
+        {
+
+            return taxaDeJurosAnual = 0.05;
+        }
+    }
+}
---
+++ Poupança/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poupança
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaPoupanca CP = new ContaPoupanca();
+
+            ContaPoupanca poupador1 = new ContaPoupanca();
+            ContaPoupanca poupador2 = new ContaPoupanca();
+
+
+            //Imprimir saldo atual.
+            Console.WriteLine("Saldos originais. ");
+            CP.SaldoPoupanca();
+
+            //Calcular o juros de 4%
+            CP.CalcularJurosMensal();
+
+            //Imprimir novo saldo com 4% / 12
+            Console.WriteLine("Seus saldos esse mês, com o juros anual em 4% e o mensal em 0,33%.");
+            CP.SaldoPoupanca();
+
+            //Alterando o juros para 5%
+            CP.AlterarTaxaDeJuros();
+
+            //Calcula o saldo com os 5%
+            CP.CalcularJurosMensal();
+
+            //Mostra o novo saldo
+            Console.WriteLine("Seus saldos no mês subsequente, com o juros anual em5% e o mensal em 0,41%.");
+            CP.SaldoPoupanca();
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
